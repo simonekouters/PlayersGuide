ea217dd6bfc644c325bd85a15b51a454f516b065
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-17 21:54:59 +0100

    Add files via upload

---
+++ 06_TheVariableShop/TheVariableShop.java
@@ -0,0 +1,35 @@
+
+public class TheVariableShop {
+
+    public static void main(String[] args) {
+
+        byte myByteNumber = 127;
+        System.out.println(myByteNumber);
+
+        short myShortNumber = -10000;
+        System.out.println(myShortNumber);
+
+        int myIntNumber = -2345678;
+        System.out.println(myIntNumber);
+
+        long myLongNumber = 12345678l;
+        System.out.println(myLongNumber);
+
+        float myFloatNumber = 1.1234567f;
+        System.out.println(myFloatNumber);
+
+        double myDoubleNumber = 1.123456789;
+        System.out.println(myDoubleNumber);
+
+        char myLetter = 'S';
+        System.out.println(myLetter);
+
+        boolean myBoolean = false;
+        System.out.println(myBoolean);
+
+        String myText = "Simone";
+        System.out.println(myText);
+
+    }
+
+}
