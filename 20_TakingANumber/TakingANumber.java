df1c3faf1ce69051aae7ab9334b62b3fc482cade
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-17 21:59:04 +0100

    Add files via upload

---
+++ 20_TakingANumber/TakingANumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class TakingANumber {
+
+    public static void main(String[] args) {
+		askForNumber("Enter a number:");
+		askForNumberInRange("Enter a number between 1 and 100", 1, 100);
+    }
+	
+    public static int askForNumber(String text) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(text);
+        int number = Integer.valueOf(scanner.nextLine());
+        return number;
+    }
+    public static int askForNumberInRange(String text, int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+		int number;
+		do {
+			System.out.println(text);
+            number = Integer.valueOf(scanner.nextLine());
+        } while (number < min || number > max);
+        return number;
+    }
+}
