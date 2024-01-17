e5d3e7c9866cbba4554e73b8ab2d015bbd385e8e
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-17 21:55:39 +0100

    Add files via upload

---
+++ 09_TheFourSistersAndTheDuckbear/TheFourSistersAndTheDuckbear.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TheFourSistersAndTheDuckbear {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many chocolate eggs were gathered today?");
+
+        int eggs = Integer.valueOf(scanner.nextLine());
+        int eggsForEachSister = eggs / 4;
+        int eggsForDuckbear = eggs % 4;
+		
+		System.out.println();
+        System.out.println("Eggs for each sister: " + eggsForEachSister);
+        System.out.println("Eggs for the duckbear: " + eggsForDuckbear);
+
+        // The duckbear gets more eggs than the sisters when the number of eggs is 0, 1, 2, 3, 6 or 11.
+    }
+}
+
+
+
