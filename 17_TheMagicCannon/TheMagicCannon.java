1bee349451ac36ed6a15a8877446afe17708d7e2
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-17 21:58:07 +0100

    Add files via upload

---
+++ 17_TheMagicCannon/TheMagicCannon.java
@@ -0,0 +1,24 @@
+public class TheMagicCannon {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            String blast = "";
+            if (i % 5 == 0 && i % 3 == 0) {
+                blast = ANSI_BLUE + "Electric and Fire" + ANSI_RESET;
+            } else if (i % 5 == 0) {
+                blast = ANSI_YELLOW + "Electric" + ANSI_RESET;
+            } else if (i % 3 == 0) {
+                blast = ANSI_RED + "Fire" + ANSI_RESET;
+            } else {
+                blast = ANSI_WHITE + "Normal" + ANSI_RESET;
+            }
+            System.out.println(i + ": " + blast);
+        }
+    }
+}
