3bfaf6c1f5deb1f6cd0f3af00eb0308ed54026ff
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-17 21:54:14 +0100

    Add files via upload

---
+++ 03_TheMakingsOfAProgrammer/TheMakingsOfAProgrammer.java
@@ -0,0 +1,9 @@
+public class TheMakingsOfAProgrammer {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hallo Wereld!");
+		System.out.println("Ciao Mondo!");
+		System.out.println("Bonjour Le Monde!");
+		System.out.println("Hej Världen!");
+	}
+}
