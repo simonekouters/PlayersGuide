ece6f541d906e65360e6ef5bd5da30c6b0c5ab8d
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-17 21:56:53 +0100

    Add files via upload

---
+++ 14_BuyingInventory/BuyingInventory.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BuyingInventory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("MENU:");
+        System.out.println("1.Rope\n" + "2.Torches\n" + "3.Climbing Equipment\n" +
+                "4.Clean Water\n" + "5.Machete\n" + "6.Canoe\n" + "7.Food Supplies\n");
+        System.out.print("What number do you want to see the price of? ");
+        int item = Integer.valueOf(scanner.nextLine());
+
+        int price = switch (item) {
+            case 1 -> 10;
+            case 2 -> 15;
+            case 3 -> 25;
+            case 4, 7 -> 1;
+            case 5 -> 20;
+            case 6 -> 200;
+            default -> throw new IllegalArgumentException("Invalid item number");
+        };
+        System.out.println("The item costs " + price + " gold.");
+    }
+}
