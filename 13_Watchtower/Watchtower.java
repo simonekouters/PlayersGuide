825b134fd7c76411dd8d0c0ef71427c9ce006dce
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-17 21:56:41 +0100

    Add files via upload

---
+++ 13_Watchtower/Watchtower.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Watchtower {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("x value: ");
+        int x = Integer.valueOf(scanner.nextLine());
+        System.out.print("y value: ");
+        int y = Integer.valueOf(scanner.nextLine());
+
+        if (x == 0 && y == 0) {
+            System.out.println("The enemy is here!");
+        } else if (x == 0 && y > 0) {
+            System.out.println("The enemy is to the north!");
+        } else if (x > 0 && y == 0) {
+            System.out.println("The enemy is to the east!");
+        } else if (x == 0 && y < 0) {
+            System.out.println("The enemy is to the south!");
+        } else if (x < 0 && y == 0) {
+            System.out.println("The enemy is to the west!");
+        } else if (x > 0 && y > 0) {
+            System.out.println("The enemy is to the northeast!");
+        } else if (x > 0 && y < 0) {
+            System.out.println("The enemy is to the southeast!");
+        } else if (x < 0 && y < 0) {
+            System.out.println("The enemy is to the southwest!");
+        } else {
+            System.out.println("The enemy is to the northwest!");
+        }
+    }
+}
