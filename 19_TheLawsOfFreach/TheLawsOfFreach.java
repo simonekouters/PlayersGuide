0181f88c03de6b04ce8cfb2939e2ba6940d49510
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-17 21:58:48 +0100

    Add files via upload

---
+++ 19_TheLawsOfFreach/TheLawsOfFreach.java
@@ -0,0 +1,19 @@
+public class TheLawsOfFreach {
+    public static void main(String[] args) {
+        int[] array = { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+        int currentSmallest = Integer.MAX_VALUE; // Start higher than anything in the array.
+		
+        for (int number : array) {
+            if (number < currentSmallest)
+                currentSmallest = number;
+        }
+        System.out.println(currentSmallest);
+
+        int total = 0;
+        for (int number : array) {
+            total += number;
+        }
+        double average = (double)total / array.length;
+        System.out.println(average);
+    }
+}
