3bcac692f70566def61c0234a7953ad7221c2af9
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-17 21:56:24 +0100

    Add files via upload

---
+++ 12_RepairingTheClocktower/RepairingTheClocktower.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class RepairingTheClocktower {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Number: ");
+        int number = Integer.valueOf(scanner.nextLine());
+
+        if (number % 2 == 0) {
+            System.out.println("Tick");
+        } else {
+            System.out.println("Tock");
+        }
+    }
+}
