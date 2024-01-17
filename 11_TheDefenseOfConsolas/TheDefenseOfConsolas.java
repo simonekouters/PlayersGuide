2721d127068ea174e7d1e714ab99408899967266
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-17 21:56:10 +0100

    Add files via upload

---
+++ 11_TheDefenseOfConsolas/TheDefenseOfConsolas.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.awt.Toolkit;
+
+public class TheDefenseOfConsolas {
+    public static final String ANSI_RED = "\u001B[31m";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Target Row? ");
+        int row = Integer.valueOf(scanner.nextLine());
+        System.out.print("Target Column? ");
+        int column = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Deploy to: ");
+        System.out.println(ANSI_RED + "(" + row + ", " + (column - 1) + ")");
+        System.out.println("(" + (row - 1) + ", " + column + ")");
+        System.out.println("(" + row + ", " + (column + 1) + ")");
+        System.out.println("(" + (row + 1) + ", " + column + ")");
+
+        Toolkit.getDefaultToolkit().beep();
+    }
+}
