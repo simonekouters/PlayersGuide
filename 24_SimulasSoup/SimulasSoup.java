edf1559730a75804ec54ec768b66ec9329f85bb1
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-21 20:30:26 +0100

    Add files via upload

---
+++ 24_SimulasSoup/SimulasSoup.java
@@ -0,0 +1,7 @@
+public class SimulasSoup {
+    public static void main(String[] args) {
+		
+		Shop shop = new Shop();
+		shop.start();
+    }
+}
---
+++ 24_SimulasSoup/Type.java
@@ -0,0 +1,28 @@
+public enum Type {
+    SOUP("Soup"), 
+	STEW("Stew"), 
+	GUMBO("Gumbo");
+	
+	private final String name;
+	
+	private Type(String name) {
+		this.name = name;
+	}
+	
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	
+	public String toString() {
+		return "- " + this.name;
+	}
+	
+	
+	public void printTypes() {
+		for (Type type : Type.values()) {
+			System.out.println(type.toString());
+		}
+	}
+}
