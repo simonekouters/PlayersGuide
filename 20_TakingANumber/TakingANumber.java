d553f4ac2b12a86718296eb4afee58c8772622ee
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-21 20:22:19 +0100

    Optimize code

--- 20_TakingANumber/TakingANumber.java
+++ 20_TakingANumber/TakingANumber.java
@@ -3,23 +3,30 @@ import java.util.Scanner;
 public class TakingANumber {
 
     public static void main(String[] args) {
+		
 		askForNumber("Enter a number:");
+		
 		askForNumberInRange("Enter a number between 1 and 100", 1, 100);
     }
 	
     public static int askForNumber(String text) {
         Scanner scanner = new Scanner(System.in);
         System.out.println(text);
-        int number = Integer.valueOf(scanner.nextLine());
+		
+        int number = Integer.parseInt(scanner.nextLine());
         return number;
     }
+	
+	
     public static int askForNumberInRange(String text, int min, int max) {
         Scanner scanner = new Scanner(System.in);
 		int number;
+		
 		do {
 			System.out.println(text);
-            number = Integer.valueOf(scanner.nextLine());
+            number = Integer.parseInt(scanner.nextLine());
         } while (number < min || number > max);
+		
         return number;
     }
 }
