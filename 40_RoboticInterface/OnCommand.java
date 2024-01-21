4e71e4d7f90526646f432b1ef945142753c7f232
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-21 20:41:05 +0100

    Add files via upload

---
+++ 40_RoboticInterface/OnCommand.java
@@ -0,0 +1,6 @@
+public class OnCommand implements RobotCommand {
+
+    public void run(Robot robot) {
+        robot.setPowered(true);
+    }
+}
---
+++ 40_RoboticInterface/WestCommand.java
@@ -0,0 +1,6 @@
+public class WestCommand implements RobotCommand {
+
+    public void run(Robot robot) {
+        robot.setX(-1);
+    }
+}
