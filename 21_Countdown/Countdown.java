996ad9cd10f1ee28808d2b2881741c87267f9477
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-21 20:23:39 +0100

    Reformat code

--- 21_Countdown/Countdown.java
+++ 21_Countdown/Countdown.java
@@ -1,7 +1,10 @@
 public class Countdown {
     public static void main(String[] args) {
+		
         countDown(10);
     }
+	
+	
     public static void countDown(int n) {
         if (n > 0) {
             System.out.println(n);
