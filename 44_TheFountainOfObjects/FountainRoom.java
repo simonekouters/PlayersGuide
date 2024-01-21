dc462b0f2307f6a1fbf7c58f5e160f42862176eb
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-21 20:47:56 +0100

    Add files via upload

---
+++ 44_TheFountainOfObjects/FountainRoom.java
@@ -0,0 +1,32 @@
+public class FountainRoom {
+	
+	private int x;
+	private int y;
+	private boolean fountain;
+	
+	public FountainRoom(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.fountain = false;
+	}
+	
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	
+	public int getY() {
+		return this.y;
+	}
+
+
+	public void enableFountain() {
+		fountain = true;
+	}
+	
+	
+	public boolean fountainIsOn() {
+		return this.fountain;
+	}
+}
---
+++ 44_TheFountainOfObjects/TheFountainOfObjects.java
@@ -0,0 +1,12 @@
+public class TheFountainOfObjects {
+	public static void main(String[] args) {
+		
+		FountainRoom fountainRoom = new FountainRoom(0, 2);
+		
+		World world = new World(4);
+		
+		UserInterface ui = new UserInterface(world, fountainRoom);
+		ui.start();
+		
+	}
+}
