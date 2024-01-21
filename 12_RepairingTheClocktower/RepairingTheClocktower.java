8f41b3acf6be7ffbca5afb3c6c680d3936483fde
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-21 20:12:00 +0100

    Optimize code

--- 12_RepairingTheClocktower/RepairingTheClocktower.java
+++ 12_RepairingTheClocktower/RepairingTheClocktower.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class RepairingTheClocktower {
     public static void main(String[] args) {
+		
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Number: ");
-        int number = Integer.valueOf(scanner.nextLine());
+        int number = Integer.parseInt(scanner.nextLine());
 
         if (number % 2 == 0) {
             System.out.println("Tick");
