3a262e3c50858030d9a2cb958aa87bbdba434630
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-21 20:33:16 +0100

    Add files via upload

---
+++ 29_TheColor/Color.java
@@ -0,0 +1,26 @@
+public class Color {
+
+    private int red;
+    private int green;
+    private int blue;
+
+    public static final  Color WHITE = new Color(255,255,255);
+    public static final  Color BLACK = new Color(0,0,0);
+    public static final  Color RED = new Color(255,0,0);
+    public static final  Color ORANGE = new Color(255,165,0);
+    public static final  Color YELLOW = new Color(255,255,0);
+    public static final  Color GREEN = new Color(0,128,0);
+    public static final  Color BLUE = new Color(0,0,255);
+    public static final  Color PURPLE = new Color(128,0,128);
+
+    public Color(int red, int green, int blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+
+
+    public String toString() {
+        return "Red: " + this.red + "   Green: " + this.green + "   Blue: " + this.blue;
+    }
+}
---
+++ 29_TheColor/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Color color1 = new Color(255, 80, 40);
+        Color color2 = Color.PURPLE;
+
+        System.out.println(color1);
+        System.out.println(color2);
+    }
+}
