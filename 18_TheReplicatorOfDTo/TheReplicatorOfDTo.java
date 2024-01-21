fb4e1657227af84df83778fcca8175e0ceb4e052
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-21 20:19:09 +0100

    Optimize code

--- 18_TheReplicatorOfDTo/TheReplicatorOfDTo.java
+++ 18_TheReplicatorOfDTo/TheReplicatorOfDTo.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class TheReplicatorOfDTo {
     public static void main(String[] args) {
+		
         Scanner scanner = new Scanner(System.in);
 
         int[] Array1 = new int[5];
@@ -11,19 +12,21 @@ public class TheReplicatorOfDTo {
             int number = askForNumber("Enter a number: ");
             Array1[i] = number;
         }
+		
         System.out.println("\nFirst Array: " + Arrays.toString(Array1));
 
         int[] Array2 = new int[5];
         for (int i = 0; i < Array1.length; i++) {
             Array2[i] = Array1[i];
         }
+		
         System.out.println("Second Array: " + Arrays.toString(Array2));
     }
 	
 	public static int askForNumber(String text) {
         Scanner scanner = new Scanner(System.in);
         System.out.print(text);
-        int number = Integer.valueOf(scanner.nextLine());
+        int number = Integer.parseInt(scanner.nextLine());
         return number;
     }
 }
