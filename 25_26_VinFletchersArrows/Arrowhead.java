da33eb099350bab09600875ccbbb51f0cd52809c
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-21 20:31:02 +0100

    Add files via upload

---
+++ 25_26_VinFletchersArrows/Arrowhead.java
@@ -0,0 +1,35 @@
+public enum Arrowhead {
+    STEEL("Steel", 10), 
+	WOOD("Wood", 3), 
+	OBSIDIAN("Obsidian", 5);
+	
+	private final String name;
+    private final int cost;
+		
+    Arrowhead (String name, int cost) {
+		this.name = name;
+        this.cost = cost;
+    }
+	
+	
+	public String getName() {
+		return this.name;
+	}
+		
+		
+	public int getCost() {
+		return this.cost;
+	}
+	
+	
+	public String toString() {
+		return "- " + this.name;
+	}
+	
+	
+	public void printArrowheadTypes() {
+		for (Arrowhead arrowhead : Arrowhead.values()) {
+			System.out.println(arrowhead.toString());
+		}
+	}
+}
---
+++ 25_26_VinFletchersArrows/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+		
+        Shop shop = new Shop();
+		shop.start();
+
+    }
+}
