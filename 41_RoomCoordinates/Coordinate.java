c51b6a2584a2a05773b1015367b610a80186e1d4
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-21 20:41:33 +0100

    Add files via upload

---
+++ 41_RoomCoordinates/Coordinate.java
@@ -0,0 +1,17 @@
+public record Coordinate(int row, int column) {
+
+    public static boolean compareCoordinates(Coordinate coordinate, Coordinate coordinate1) {
+		
+        if (((coordinate.column + 1) == coordinate1.column || (coordinate.column - 1) == coordinate1.column) &&
+        coordinate.row == coordinate1.row) {
+            return true;
+        }
+        if (((coordinate.row + 1) == coordinate1.row || (coordinate.row - 1) == coordinate1.row) &&
+                coordinate.column == coordinate1.column) {
+            return true;
+        }
+        return false;
+    }
+}
+
+
---
+++ 41_RoomCoordinates/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Coordinate coordinate1 = new Coordinate(1, 6);
+        Coordinate coordinate2 = new Coordinate(2, 6);
+        Coordinate coordinate3 = new Coordinate(2, 7);
+		
+        System.out.println(Coordinate.compareCoordinates(coordinate1, coordinate2)); //true
+		
+        System.out.println(Coordinate.compareCoordinates(coordinate1, coordinate3)); //false
+    }
+}
