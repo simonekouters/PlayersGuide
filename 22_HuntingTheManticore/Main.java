96cb956d7852956792026c01d9c484b1cd6ab140
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-21 20:28:59 +0100

    Add files via upload

---
+++ 22_HuntingTheManticore/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+		
+		ManticoreHunting hunting = new ManticoreHunting();
+		
+        UserInterface ui = new UserInterface(hunting);
+        ui.start();
+    }
+}
