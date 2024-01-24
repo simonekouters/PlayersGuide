271bd30adbf989ea40ebc2985f2b53e605b34fc9
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-24 15:39:46 +0100

    Reformat code

--- 23_SimulasTest/SimulasTest.java
+++ 23_SimulasTest/SimulasTest.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class SimulasTest {
     public static void main(String[] args) {
-		
+        
         Scanner scanner = new Scanner(System.in);
         State boxState = State.LOCKED;
 
         while (true) {
             System.out.print("The chest is " + boxState.getName() + "." + " What do you want to do? ");
             String command = scanner.nextLine();
-			
+            
             if (boxState.equals(State.OPEN) && command.equals("close")) {
                 boxState = State.CLOSED;
             } else if (boxState.equals(State.CLOSED) && command.equals("open")) {
@@ -19,8 +19,8 @@ public class SimulasTest {
             } else if (boxState.equals(State.LOCKED) && command.equals("unlock")) {
                 boxState = State.CLOSED;
             } else {
-				System.out.println("You can't " + command + " the chest, because it's " + boxState.getName() + ".");
-			}
+                System.out.println("You can't " + command + " the chest, because it's " + boxState.getName() + ".");
+            }
         }
     }
 }
