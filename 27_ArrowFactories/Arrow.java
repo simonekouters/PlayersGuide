4908c48ef5d1fe6ae38eaa4a564ea08fcc644ec9
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-01-24 15:49:03 +0100

    Reformat code

--- 27_ArrowFactories/Arrow.java
+++ 27_ArrowFactories/Arrow.java
@@ -1,60 +1,59 @@
 public class Arrow {
-
     private Arrowhead arrowhead = Arrowhead.STEEL;
-	private Fletching fletching = Fletching.PLASTIC;
+    private Fletching fletching = Fletching.PLASTIC;
     private int length;
-	private final float COST_PER_CENTIMETER; 
+    private final float COST_PER_CENTIMETER; 
 
     public Arrow(Arrowhead arrowhead, Fletching fletching, int length) {
         this.arrowhead = arrowhead;
-		this.fletching = fletching;
-		this.length = length;
-		this.COST_PER_CENTIMETER = 0.05f;
+        this.fletching = fletching;
+        this.length = length;
+        this.COST_PER_CENTIMETER = 0.05f;
     }
-	
-	
-	public Arrowhead getArrowhead() {
-		return this.arrowhead;
-	}
-	
-	
-	public Fletching getFletching() {
-		return this.fletching;
-	}
-	
-	
-	public int getLength() {
-		return this.length;
-	}
-	
-	
+    
+    
+    public Arrowhead getArrowhead() {
+        return this.arrowhead;
+    }
+    
+    
+    public Fletching getFletching() {
+        return this.fletching;
+    }
+    
+    
+    public int getLength() {
+        return this.length;
+    }
+    
+    
     public static Arrow createEliteArrow() {
         Arrow eliteArrow = new Arrow(Arrowhead.STEEL, Fletching.PLASTIC, 95);
         return eliteArrow;
     }
-	
-	
+    
+    
     public static Arrow createBeginnerArrow() {
         Arrow beginnerArrow = new Arrow(Arrowhead.WOOD, Fletching.GOOSEFEATHERS, 75);
         return beginnerArrow;
     }
-	
-	
+    
+    
     public static Arrow createMarksmanArrow() {
         Arrow marksmanArrow = new Arrow(Arrowhead.WOOD, Fletching.GOOSEFEATHERS, 65);
         return marksmanArrow;
     }
-	
-	
+    
+    
     public float getCost() {
         int arrowCost = this.arrowhead.getCost(); 
-		
+        
         int fletchingCost = this.fletching.getCost(); 
-		
+        
         float lengthCost = this.length * COST_PER_CENTIMETER;
-		
-		float totalCost = arrowCost + fletchingCost + lengthCost;
-		
+        
+        float totalCost = arrowCost + fletchingCost + lengthCost;
+        
         return totalCost;
     }
 }
--- 27_ArrowFactories/Fletching.java
+++ 27_ArrowFactories/Fletching.java
@@ -1,35 +1,35 @@
 public enum Fletching {
     PLASTIC("Plastic", 10), 
-	TURKEYFEATHERS("Turkey feathers", 5), 
-	GOOSEFEATHERS("Goose feathers", 3);
+    TURKEYFEATHERS("Turkey feathers", 5), 
+    GOOSEFEATHERS("Goose feathers", 3);
 
-	private final String name;
+    private final String name;
     private final int cost;
-		
+        
     Fletching (String name, int cost) {
-		this.name = name;
+        this.name = name;
         this.cost = cost;
     }
-	
-	
-	public String getName() {
-		return this.name;
-	}
-		
-		
-	public int getCost() {
-		return this.cost;
-	}
-	
-	
-	public String toString() {
-		return "- " + this.name;
-	}
-	
-	
-	public void printFletchingTypes() {
-		for (Fletching fletching : Fletching.values()) {
-			System.out.println(fletching.toString());
-		}
-	}
+    
+    
+    public String getName() {
+        return this.name;
+    }
+        
+        
+    public int getCost() {
+        return this.cost;
+    }
+    
+    
+    public String toString() {
+        return "- " + this.name;
+    }
+    
+    
+    public void printFletchingTypes() {
+        for (Fletching fletching : Fletching.values()) {
+            System.out.println(fletching.toString());
+        }
+    }
 }
