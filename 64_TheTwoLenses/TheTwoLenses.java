e6b4c4eecc4c990cb5aa8747194a7d81ce19d2b1
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-03-06 19:32:36 +0100

    Add file

---
+++ 64_TheTwoLenses/TheTwoLenses.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[] {1, 9, 2, 8, 3, 7, 4, 6, 5};
+        
+        System.out.println(sortArray(array));
+        System.out.println(sortArrayWithLambda(array));
+    }
+    
+    private static List<Integer> sortArray(int[] array) {
+        Arrays.sort(array);
+        List<Integer> list = new ArrayList<>();
+        
+        for (int number : array) {
+            if (number % 2 == 0) {
+                list.add(number * 2);
+            }
+        }
+        return list;
+  }
+
+    private static List<Integer> sortArrayWithLambda(int[] array) {
+        return Arrays.stream(array)
+            .sorted()
+            .filter(number -> number % 2 == 0)
+            .mapToObj(number -> number * 2)
+            .collect(Collectors.toList());
+    }
+}
+
